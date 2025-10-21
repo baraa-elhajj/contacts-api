9be1431502467129bcd2b67a84615636c7173a57
Baraa El Hajj <baraa.w.hajj@gmail.com>
2025-10-21 23:38:42 +0300

    Create errorHandler

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require("express");
+const errorHandler = require("./middleware/errorHandler");
 require("dotenv").config();
 
 const port = process.env.PORT || 5000;
 const app = express();
 
+// To accept json request bodies
+app.use(express.json());
+
+// To include all routes from contactRoutes
 app.use("/api/contacts", require("./routes/contactRoutes"));
+
+// To use errorHandler middleware
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log("Server is running on port ", port);
