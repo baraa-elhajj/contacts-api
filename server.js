ab9383d12918e75c6fc6ca9873d02d9f5b00470f
Baraa El Hajj <baraa.w.hajj@gmail.com>
2025-10-21 22:26:52 +0300

    Create contactRoutes
    used contact routes

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ require("dotenv").config();
 const port = process.env.PORT || 5000;
 const app = express();
 
+app.use("/api/contacts", require("./routes/contactRoutes"));
+
 app.listen(port, () => {
   console.log("Server is running on port ", port);
 });
