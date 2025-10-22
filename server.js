c260320bd65fc073bb35518fa8bce1f564d0b655
Baraa El Hajj <baraa.w.hajj@gmail.com>
2025-10-22 15:45:40 +0300

    Create dbConnection

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const connectDb = require("./config/dbConnection");
 const errorHandler = require("./middleware/errorHandler");
 require("dotenv").config();
 
 const port = process.env.PORT || 5000;
 const app = express();
+connectDb();
 
 // To accept json request bodies
 app.use(express.json());
