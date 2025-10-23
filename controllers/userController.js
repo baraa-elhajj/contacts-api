fab90103ede4a7ae02f39c3f3a48e987bb42c1b3
Baraa El Hajj <baraa.w.hajj@gmail.com>
2025-10-23 03:43:14 +0300

    Create userController.js

---
+++ controllers/userController.js
@@ -0,0 +1,81 @@
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+//@desc Register user
+//@route POST /api/users/register
+//@access public
+const registerUser = asyncHandler(async (req, res) => {
+  const { username, email, password } = req.body;
+  if (!username || !email || !password) {
+    res.status(400);
+    throw new Error("All fields are mandatory.");
+  }
+
+  const userExists = await User.findOne({ username });
+  if (userExists) {
+    res.status(400);
+    throw new Error("Username already exists.");
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const user = await User.create({
+    username,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(201).json({ _id: user.id, username: user.username });
+  } else {
+    res.status(400);
+    throw new Error("Invalid Data.");
+  }
+});
+
+//@desc Login user
+//@route POST /api/users/login
+//@access public
+const loginUser = asyncHandler(async (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    res.status(400);
+    throw new Error("All fields are mandatory.");
+  }
+
+  const user = await User.findOne({ username });
+  if (user && (await bcrypt.compare(password, user.password))) {
+    const accessToken = jwt.sign(
+      {
+        user: {
+          username: user.username,
+          email: user.email,
+          id: user._id,
+        },
+      },
+      process.env.ACCESS_TOKEN_SECRET ?? "",
+      { expiresIn: "1m" }
+    );
+
+    res.status(200).json({ accessToken });
+  } else {
+    res.status(401);
+    throw new Error("Invalid username or password.");
+  }
+
+  res.status(201);
+});
+
+//@desc Get current logged in user
+//@route GET /api/users/current
+//@access private
+const currentUser = asyncHandler(async (req, res) => {
+  res.status(200);
+});
+
+module.exports = {
+  registerUser,
+  loginUser,
+  currentUser,
+};
